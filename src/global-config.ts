5fc6cf2c342a535f4a75e1283d0c7e22c3a7175f
finanzabudgetapp-hub <finanzabudgetapp@gmail.com>
2025-11-06 18:39:08 +0100

    Update API base URL default value in config

--- src/global-config.ts
+++ src/global-config.ts
@@ -30,9 +30,10 @@ export const GLOBAL_CONFIG: GlobalConfig = {
 	appVersion: packageJson.version,
 	defaultRoute: import.meta.env.VITE_APP_DEFAULT_ROUTE || "/workbench",
 	publicPath: import.meta.env.VITE_APP_PUBLIC_PATH || "/",
-	apiBaseUrl: import.meta.env.VITE_APP_API_BASE_URL || "/.netlify/functions",
+	apiBaseUrl: import.meta.env.VITE_APP_API_BASE_URL || "/api",
 	routerMode: import.meta.env.VITE_APP_ROUTER_MODE || "frontend",
 };
+
 
 
 
