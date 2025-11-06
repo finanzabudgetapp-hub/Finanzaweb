c670cf3cb37870a66b9eb5eea8d274e53d248f62
finanzabudgetapp-hub <finanzabudgetapp@gmail.com>
2025-11-06 14:28:07 +0100

    Add type definition for handler in auth-signin

--- src/netlify/functions/auth-signin.ts
+++ src/netlify/functions/auth-signin.ts
@@ -1,4 +1,6 @@
-export const handler = async (event) => {
+import type { Handler } from "@netlify/functions";
+
+export const handler: Handler = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Signed in (mock)" }),
